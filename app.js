799a3812f13c84f1c2e75cf13f7ead5fdcef6c3a
kadalsaune <kadalsaune@gmail.com>
2025-12-12 10:43:51 +0100

    Implement logout button

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const personerRouter = require('./routes/personer');
 const bilerRouter = require('./routes/biler');
 const loginRouter = require('./routes/login');
 const beskyttetRouter = require('./routes/beskyttet');
+const logoutRouter = require('./routes/logout')
 
 const app = express();
 
@@ -37,5 +38,6 @@ app.use('/personer', personerRouter);
 app.use('/biler', bilerRouter);
 app.use('/login', loginRouter);
 app.use('/beskyttet', beskyttetRouter);
+app.use('/logout', logoutRouter);
 
 module.exports = app;
--- routes/beskyttet.js
+++ routes/beskyttet.js
@@ -5,7 +5,9 @@ const kreverInnlogging = require('../auth.js')
 const bcrypt = require('bcrypt');
 
 router.get("/", kreverInnlogging, (req, res) => {
-    res.send(`Velkommen, ${req.session.bruker.fornavn}! Dette er en beskyttet side.`)
+    res.send(`<p>Velkommen, ${req.session.bruker.fornavn}! Dette er en beskyttet side.</p>
+        <button onclick="loggut()">Logg ut</button>
+        <script src="/javascripts/logoutHandler.js"></script>`)
 });
 
 module.exports = router;
